b069043126353fd711161a38304c8c6da6a00554
istar <istar@192.168.100.8>
2017-05-25 10:51:24 +0530

    basic calc is completed by sunitha jsp2 warangal

--- src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
@@ -1,49 +1,109 @@
 package com.onlinecalculator.calculator.calctypes;
+
 import java.util.Scanner;
 
-public class BasicCalc
+class Calc1
 {
+	int a,b,c,ch,res;
 	
-	public BasicCalc()
-        {
-		
-	}
+	public Calc1( )
+        { // hello
+		a=0;
+		b=0;
+		c=0;
+		ch=0;
+		res=0;
+		}
 	
 	public int menu()
         {
-    	
+		System.out.println("1.addition");
+		System.out.println("2.substraction");
+		System.out.println("3.mul");
+		System.out.println("4.div");
+		System.out.println("enter your chioce");
+		Scanner sc=new Scanner(System.in);
+	    ch=sc.nextInt();
+	    return 0;
+	    
 	}
 	
     public void userInputs()
     {
-    	
-    	
+    	Scanner s=new Scanner(System.in);
+    	System.out.println(" Enter first number");
+    	a=s.nextInt();
+    	System.out.println(" Enter second number");
+    	b=s.nextInt();
+    	   	
     }
 
     public void performOperation()
-{
+    	{
     	
+    	 switch(ch)
+ 	    {
+ 	  case 1:
+ 	    System.out.println("addition");
+ 	    res=add();
+ 	    System.out.println(res);
+ 	    break;
+ 	  case 2:
+ 	    System.out.println("subtaction");
+ 	    res=subtract();
+ 	    System.out.println(res);
+ 	    break;
+ 	  case 3:
+ 		  System.out.println("mul");
+ 		  res=multiply();
+ 		  System.out.println(res);
+ 		  break;
+ 	  case 4:
+ 		  System.out.println("div");
+ 		  res=divide();
+ 		  System.out.println(res);
+ 	    	break;
+     	
         }
        
     }
     
 	int add()
+	
 	{
+		c=a+b;
+		return c;
 		
 	}
 	
 	int subtract()
 	{
-		
+	    c=a-b;
+	    return c;
+	
 	}
 	
 	int multiply()
 	{
-		
+	   c=a*b;
+	   return c;
 	}
 	
 	int divide()
 	{
+		 c=a/b;
+		 return c;
 	}
 	
 }
+public class BasicCalc
+{
+	public static void main(String agrs[])
+	{
+		Calc1 c=new Calc1();
+		c.menu();
+		c.userInputs();
+		c.performOperation();
+		
+	}
+	}
